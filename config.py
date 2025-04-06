2dc25b73e46394bd3d8c65832de7d99936160d1b
vampirexgirl <vampirexgirl750@gmail.com>
2025-04-06 16:59:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION = os.getenv("SESSION")
+
+PROMO_TEXT = "Please use our new bot: @Ayesha_xtri_2moviebot for latest Movies & Webseries."
