462274d2adbe9934996a49ec97b0f13e624d8271
vampirexgirl <vampirexgirl750@gmail.com>
2025-04-07 00:08:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
 
 PROMO_TEXT = "Please use our new bot: @Ayesha_xtri_2moviebot for latest Movies & Webseries."
+
+DELETE_AFTER = 10  # Seconds after reply delete
+SAFE_DELAY = 2  # Minimum safe delay for Global Anti Flood
